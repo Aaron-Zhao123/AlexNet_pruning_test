46e1b11af76990829f3eec256784d2d74df70cea
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-06-22 21:20:20 +0100

    editing

--- cifar10.py
+++ cifar10.py
@@ -154,7 +154,7 @@ def distorted_inputs():
     raise ValueError('Please supply a data_dir')
   # data_dir = os.path.join(FLAGS.data_dir, 'cifar-10-batches-bin')
 
-  dataset = ImagenetData(subset=FLAgs.subset)
+  dataset = ImagenetData(subset=FLAGS.subset)
   assert dataset.data_files()
   if tf.gfile.exists(flags.train_dir):
     tf.gfile.deleterecursively(flags.train_dir)
